c9ac9a8a4a994de579ce1fc372505b5081cf90ab
chisaiki <mishitarahman@gmail.com>
2025-09-23 16:41:14 -0400

    started website

---
+++ server/data/cities.js
@@ -0,0 +1,52 @@
+const cities = [
+   {
+    name: "Hiroshima",
+    rank: "1",
+    description: "A city known for its peace memorial and easily the most calm and serene city I've visited; with a perfect blend of nature and modern architecture.",
+    hotel:"The Knot",
+    hotellocation: "Hiroshima City",
+    hotelimage: "hotelimages/theknot.png",
+    image: "cityimages/hiroshima.jpg"
+  },
+  {
+    name: "Kanazawa",
+    rank: "2",
+    description: "The local have kindness like no other, such welcoming attitude filled with excitement and curiosity.",
+    hotel:"Apa Hotel Kanazawa Nomachi",
+    hotellocation: "Ishikawa",
+    hotelimage: "hotelimages/apa.png",
+    image: "cityimages/kanazawa.jpg"
+  },
+  {
+    name: "Tokyo",
+    rank: "4",
+    description: "The bustling capital of Japan, but its the corner alleys you want to explore the most. Ranging from strange to absurd themed foods and entertainment.",
+    hotel:"スパーホテル (Super Hotel)",
+    hotellocation: "Ueno",
+    hotelimage: "hotelimages/supahotel.png",
+    image: "cityimages/tokyo.jpg"
+  },
+  {
+    name: "Kyoto",    
+    rank: "5",
+    description: "Famous for its temples and gardens but stay for the flea market filled with old relics of Japan and the vendors waiting to discuss the history.",
+    hotel:"Fujitaya BnB",
+    hotellocation: "Shimogyo Ward",
+    hotelimage: "hotelimages/fujitaya.png",
+    image: "cityimages/kyoto.jpg"
+  },
+  {
+    name: "Nara",
+    rank: "3",
+    description: "Often known for their bowing deers, but also have the kindest souls who want to perserve their city and its history.",
+    hotel:"Centurion Hotel Classic Nara",
+    hotellocation: "Nara",
+    hotelimage: "hotelimages/centurion.png",
+    image: "cityimages/nara.jpg"
+  }
+];
+
+/*i can store the data in a simple array in the backend
+and turn them into cards using front end methods*/
+
+module.exports = cities;
